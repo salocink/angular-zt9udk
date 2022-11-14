552e1fe1569169eb2dc7cf46bf97e951cc6f67a0
salocink <32554258+salocink@users.noreply.github.com>
2022-11-13 21:54:34 -0500

    test

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
 
 
-/*
+/* test
 Copyright Google LLC. All Rights Reserved.
 Use of this source code is governed by an MIT-style license that
 can be found in the LICENSE file at https://angular.io/license
